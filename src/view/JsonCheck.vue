0ef75a2dc50b46e67808eb68043c36959a3f1c36
Dggrdg <kobe640670@gmail.com>
2025-09-16 17:58:45 +0800

    2025091602

--- src/view/JsonCheck.vue
+++ src/view/JsonCheck.vue
@@ -6,20 +6,20 @@
           type="file"
           class="form-control col-4"
           aria-describedby="basic-addon1"
-          @change="selectFile"
+          accept="json"
+          @change="handleFile($event)"
         />
       </div>
       <div class="col-4" style="display: flex; gap: 20px">
         <button type="button" class="btn uploadbtn">上傳</button>
-        <button type="button" class="btn clearbtn">清空</button>
       </div>
-      <div class="col-12" style="display: flex; align-content: center">
-        <p class="hint">僅能選擇JSON檔</p>
+      <div class="col-12 d-flex align-items-center" style="height: 40px">
+        <p class="hint m-0">僅能選擇JSON檔</p>
       </div>
       <div class="col-5">
         <textarea
           disabled
-          class="form-control mt-4"
+          class="form-control"
           id="exampleFormControlTextarea1"
           rows="3"
         ></textarea>
@@ -29,9 +29,13 @@
 </template>
 
 <script lang = "ts" setup>
-// import { ref } from "vue";
+let jsonFile: any = null;
 
-// const file = ref();
+function handleFile(event: Event) {
+  jsonFile = event.target;
 
-const selectFile = () => {};
+  console.log(jsonFile);
+}
+
+//重置檔案選擇器
 </script>
